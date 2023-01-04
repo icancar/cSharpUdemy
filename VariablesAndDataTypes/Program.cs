ebd0cec2f5eda382dcf67acc1815935ea8e493ab
Igor <icancar99@gmail.com>
2023-01-04 15:08:22 +0100

    Add one variable and print out value

---
+++ VariablesAndDataTypes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VariablesAndDataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            num1 = 5;
+            Console.Write(num1);
+
+        }
+    }
+}
