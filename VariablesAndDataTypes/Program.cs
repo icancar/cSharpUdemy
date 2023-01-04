a5155807385eac9860fae6c9ea9045d677a6cdff
Igor <icancar99@gmail.com>
2023-01-04 15:49:51 +0100

    Add String manipulation

--- VariablesAndDataTypes/Program.cs
+++ VariablesAndDataTypes/Program.cs
@@ -9,20 +9,23 @@ namespace VariablesAndDataTypes
             int num1;
             num1 = 5;
             int num2 = 1;
-            Console.WriteLine("Sum of "+ num1+ " and "+num2+" is: "+(num1+num2));
+            //Console.WriteLine("Sum of "+ num1+ " and "+num2+" is: "+(num1+num2));
 
             //multiple variables
             double a, b, c;
             a = 5;
             b = 2.67;
-            Console.WriteLine("A = " + a+ ", B = " + b+" = "+ (a/b));
+            //Console.WriteLine("A = " + a+ ", B = " + b+" = "+ (a/b));
 
             //floating point variables
             float f1 = 5.02f;
             float f2 = 2.22f;
             float fDiv = f1 / f2;
-            Console.WriteLine("F1/F2 = " + fDiv);
+            //Console.WriteLine("F1/F2 = " + fDiv);
 
+            //String manipulation
+            String myName = "Igor";
+            Console.WriteLine(myName);
 
         }
     }
