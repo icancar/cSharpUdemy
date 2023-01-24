43ef1450f5fee948fe086e95c89f0e2ba98ff528
Igor <icancar99@gmail.com>
2023-01-24 22:41:36 +0100

    add challenge 2

--- VariablesAndDataTypes/Program.cs
+++ VariablesAndDataTypes/Program.cs
@@ -11,10 +11,21 @@ namespace VariablesAndDataTypes
             Console.WriteLine("Please enter your name and press enter");
             myName = Console.ReadLine();
 
-            Console.WriteLine(myName.ToLower());
-            Console.WriteLine(myName.ToUpper());
+            Console.WriteLine("My name in lowercase: "+myName.ToLower());
+            Console.WriteLine("My name in uppercase: "+myName.ToUpper());
             //clear t
-            Console.WriteLine(myName.Trim());
+            Console.WriteLine("My name without whitespaces {{0}}",myName.Trim());
+
+            //Challenge 2
+
+            string myString;
+            Console.WriteLine("Enter a string here: ");
+            myString = Console.ReadLine();
+            Console.WriteLine("Enter a character to search:");
+            char characterToSearch = (char)Console.Read();
+            Console.WriteLine("Index of first occurence of character {0} is {1}", characterToSearch, myString.IndexOf(characterToSearch));
+
+
 
 
         }
