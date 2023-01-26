5729c263aac1ee07f3ae7864092c28045161aaca
Igor <icancar99@gmail.com>
2023-01-26 20:10:19 +0100

    Add function with one argument

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -7,11 +7,18 @@ namespace Functions
         static void Main(string[] args)
         {
             WriteSomething();
+            WriteSomethingSpecific("This is an argument, called from method!");
         }
 
         public static void WriteSomething()
         {
             Console.WriteLine("I am called from Main Method.");
+            Console.Read();
+        }
+
+        public static void WriteSomethingSpecific(string stringToBeWritten)
+        {
+            Console.WriteLine(stringToBeWritten);
             Console.Read();
         }
     }
