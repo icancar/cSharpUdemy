d45e256b658dfbd4add741bb6386b7ced2606b80
Igor <icancar99@gmail.com>
2023-01-26 20:06:59 +0100

    Add first method and call

---
+++ Functions/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteSomething();
+        }
+
+        public static void WriteSomething()
+        {
+            Console.WriteLine("I am called from Main Method.");
+            Console.Read();
+        }
+    }
+}
