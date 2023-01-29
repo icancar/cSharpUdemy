63ad6baa3abaa4ac5964adef186230ef700d0bdb
Igor <icancar99@gmail.com>
2023-01-29 18:23:22 +0100

    Make basic If-Else statement

---
+++ Decisions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Decisions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter temperature!");
+            int currentTemperature = int.Parse(Console.ReadLine());
+
+            if(currentTemperature <=10)
+            {
+                Console.WriteLine("It's so cold!");
+            }
+            else if (currentTemperature >=11 && currentTemperature <=20)
+            {
+                Console.WriteLine("It's little bit warmer!");
+            }
+            else
+            {
+                Console.WriteLine("It's so warm outside!");
+            }
+        }
+    }
+}
