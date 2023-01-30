8aa65f96002e649cf9252faf73a0a10a6ab7ec89
Igor <icancar99@gmail.com>
2023-01-30 20:24:53 +0100

    Implement first for loop

---
+++ Loops/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Current value of i is {0}",i);
+            }
+        }
+    }
+}
