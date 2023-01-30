a79ee11916d82a62220400e9d84d198d590e44cd
Igor <icancar99@gmail.com>
2023-01-30 20:28:20 +0100

    Print out odd numbers from 0 to 20

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -6,9 +6,17 @@ namespace Loops
     {
         static void Main(string[] args)
         {
-            for(int i = 0; i < 3; i++)
+            for (int i = 0; i < 3; i++)
             {
-                Console.WriteLine("Current value of i is {0}",i);
+                Console.WriteLine("Current value of i is {0}", i);
+            }
+
+            for (int i = 0; i <= 20; i++)
+            {
+                if(i%2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
             }
         }
     }
