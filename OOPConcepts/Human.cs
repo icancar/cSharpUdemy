366fd2d4de4c5558b7400787981dd78a21c8c3b2
Igor <icancar99@gmail.com>
2023-02-03 23:12:48 +0100

    Add new class and one method

---
+++ OOPConcepts/Human.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConcepts
+{
+    internal class Human
+    {
+        //members
+        public string firstName;
+
+
+        //member
+        public void IntroduceMyself()
+        {
+            Console.WriteLine("Hi, I am {0}", firstName);
+        }
+
+    }
+}
---
+++ OOPConcepts/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace OOPConcepts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create object of class Human
+            Human denis = new Human();
+            denis.firstName = "Denis";
+
+            denis.IntroduceMyself();
+
+            
+        }
+    }
+}
