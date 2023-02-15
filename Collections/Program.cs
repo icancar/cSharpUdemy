bfa8087ce2f481b7421f2e9f68bd9fb8aa3a8e78
Igor <icancar99@gmail.com>
2023-02-15 16:22:02 +0100

    Pass array as argument to function

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -11,7 +11,20 @@ namespace Collections
             matrix.initializeMatrix();
 
             matrix.printMatrix();
+            int[] studentGrades = new int[] { 2, 2, 33, 4, 5 };
+            Console.WriteLine("AverageValue of array is: {0}", getAverageValue(studentGrades));
 
+        }
+
+        static double getAverageValue(int[] gradesArray)
+        {
+            double sum = 0;
+            for(int i = 0; i< gradesArray.Length; i++)
+            {
+                sum += gradesArray[i];
+            }
+
+            return (double)sum/gradesArray.Length;
         }
     }
 }
