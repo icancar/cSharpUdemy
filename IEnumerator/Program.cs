cd1e8598d4569c58ad572ef6606b0d55e1d4f269
Igor <icancar99@gmail.com>
2023-03-12 12:12:36 +0100

    Implement interface method isEqual

---
+++ IEnumerator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IEnumerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ticket ticket1 = new Ticket(10);
+            Ticket ticket2 = new Ticket(10);
+            Ticket ticket3 = new Ticket(20);
+
+            Console.WriteLine(ticket1.Equals(ticket2));
+            Console.WriteLine(ticket2.Equals(ticket3));
+        }
+    }
+}
---
+++ IEnumerator/Ticket.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumerator
+{
+    class Ticket : IEquatable<Ticket>
+    {
+        public int DurationHours { get; set; }
+
+        public Ticket(int durationHours)
+        {
+            DurationHours = durationHours;
+        }
+
+        public bool Equals(Ticket other)
+        {
+            bool isEqual = false;
+            if(this.DurationHours == other.DurationHours) { 
+                isEqual = true;
+            }
+            return isEqual;
+        }
+    }
+}
